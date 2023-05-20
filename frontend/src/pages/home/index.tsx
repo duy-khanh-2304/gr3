b381d3901d0c3d4284dfebe1ae33f1e27a745774
Duy Khanh <ngokhanh357@gmail.com>
2023-05-20 15:49:42 +0700

    add homePage in BE

---
+++ frontend/src/pages/home/index.tsx
@@ -0,0 +1,14 @@
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+
+function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.replace('/');
+  }, []);
+
+  return null;
+}
+
+export default Home;
--- frontend/src/pages/news/index.tsx
+++ frontend/src/pages/news/index.tsx
@@ -1,9 +1,20 @@
+import axios from 'axios'
 import React from 'react'
 
-export default function News() {
+export default function News(props: any) {
+  console.log("Data: ", props.data)
   return (
     <div>
       Duy Khanh
     </div>
   )
+}
+
+export async function getStaticProps(){
+  const response = await axios.get('http://localhost:1337/api/newses?populate=deep')
+  return {
+    props: {
+      data: response.data
+    }
+  }
 }
