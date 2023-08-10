a94a6cb97d32925ed8556ba8a5ccd3c1136b4325
nghiant6 <nghia.nguyen@dssolution.jp>
2023-08-11 01:02:31 +0700

    FIX deploy

--- frontend/src/axiosConfig.ts
+++ frontend/src/axiosConfig.ts
@@ -1,10 +1,11 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: process.env.BASE_URL_BE ?? "https://6e09-3-27-123-191.ngrok-free.app/",
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL_BE,
   headers: {
-    "Content-Type": "application/json"
-  }
+    "Content-Type": "application/json",
+    "ngrok-skip-browser-warning": "69420",
+  },
 })
 
 export default axiosInstance
